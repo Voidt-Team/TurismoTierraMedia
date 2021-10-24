c6fff25356b653a5cc4a8e561193cc3bd1d27e67
Sergito <Sergito@DESKTOP-MT99LHR>
2021-10-24 19:27:30 -0300

    se crea el test atraccionDAOTest

---
+++ src/test/java/TurismoTierraMedia/dao/AtraccionDAOTest.java
@@ -0,0 +1,16 @@
+package TurismoTierraMedia.dao;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class AtraccionDAOTest {
+
+	@Test
+	public void testfindbyid()throws SQLException {
+		fail("Not yet implemented");
+	}
+
+}
