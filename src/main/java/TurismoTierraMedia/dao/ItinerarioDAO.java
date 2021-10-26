f9820a01bbebccf99b520b50c6ffe7555c5d1dfe
Sergito <Sergito@DESKTOP-MT99LHR>
2021-10-26 20:06:19 -0300

    insert en itinerarioDAO

--- src/main/java/TurismoTierraMedia/dao/ItinerarioDAO.java
+++ src/main/java/TurismoTierraMedia/dao/ItinerarioDAO.java
@@ -1,17 +1,34 @@
 package TurismoTierraMedia.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.time.LocalDateTime;
 import TurismoTierraMedia.Atraccion;
 import TurismoTierraMedia.Itinerario;
 import TurismoTierraMedia.Promocion;
+import TurismoTierraMedia.db.ConnectionProvider;
 
 
 public class ItinerarioDAO {
 
+	//aca se inserta un registro en la tabla itinerario, luego debemos usar esto para modificar usuario
+	private void insert() throws SQLException{
+		Connection connection = ConnectionProvider.getConnection();
+		String query = "INSERT INTO itineratio(\"fecha\") VALUES (?)";
+
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		//obtengo la fecha del sistema, la casteo a string y se la paso a la query
+		preparedStatement.setString(1,LocalDateTime.now().toString());
+		preparedStatement.executeUpdate();
+		
+		//deberia llamar a toItinerario?
+	}
+	
 	private void insert (Atraccion at) throws SQLException{
 		//aca deberia llamar a toItineraio y pasarle los datos para generar el objeto
 		//debo generar un registro en itinerario
