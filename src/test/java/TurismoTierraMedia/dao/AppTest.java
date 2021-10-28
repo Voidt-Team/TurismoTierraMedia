46e8d5b5fa089880ba76e285284a32080ffc0a19
Pika <pikachechi@gmail.com>
2021-10-27 22:43:04 -0300

    Cambie el nombre a al clase test, pero sigue teniendo errores porque era lectura de archivos, deberiamos acomodarla para que no tire eror

--- src/test/java/TurismoTierraMedia/dao/AppTest.java
+++ src/test/java/TurismoTierraMedia/dao/AppTest.java
@@ -9,7 +9,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TurismoTierraMediaTest {
+public class AppTest {
 
 	Atraccion prueba;
 	List<Atraccion> listaAtracciones;
